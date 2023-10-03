ae83832cbc9d5debf11db46a164e40b7393be4c2
cborjamoreno <cesarborjamoreno@gmail.com>
2023-10-03 15:04:38 +0200

    lab2 files

---
+++ labSession2/plotData.py
@@ -0,0 +1,176 @@
+#####################################################################################
+#
+# MRGCV Unizar - Computer vision - Laboratory 2
+#
+# Title: Homography, Fundamental Matrix and Two View SfM
+#
+# Date: 16 September 2022
+#
+#####################################################################################
+#
+# Authors: Jesus Bermudez, Richard Elvira, Jose Lamarca, JMM Montiel
+#
+# Version: 1.0
+#
+#####################################################################################
+
+from mpl_toolkits.mplot3d import Axes3D
+
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+
+
+# Ensamble T matrix
+def ensamble_T(R_w_c, t_w_c) -> np.array:
+    """
+    Ensamble the a SE(3) matrix with the rotation matrix and translation vector.
+    """
+    T_w_c = np.zeros((4, 4), dtype=np.float32)
+    T_w_c[0:3, 0:3] = R_w_c
+    T_w_c[0:3, 3] = t_w_c
+    T_w_c[3, 3] = 1
+    return T_w_c
+
+
+def plotLabeledImagePoints(x, labels, strColor,offset):
+    """
+        Plot indexes of points on a 2D image.
+         -input:
+             x: Points coordinates.
+             strColor: Color of the text.
+             offset: Offset from the point to the text.
+         -output: None
+         """
+    for k in range(x.shape[1]):
+        plt.text(x[0, k]+offset[0], x[1, k]+offset[1], labels[k], color=strColor)
+
+
+def plotNumberedImagePoints(x,strColor,offset):
+    """
+        Plot indexes of points on a 2D image.
+         -input:
+             x: Points coordinates.
+             strColor: Color of the text.
+             offset: Offset from the point to the text.
+         -output: None
+         """
+    for k in range(x.shape[1]):
+        plt.text(x[0, k]+offset[0], x[1, k]+offset[1], str(k), color=strColor)
+
+
+def plotLabelled3DPoints(ax, X, labels, strColor, offset):
+    """
+        Plot indexes of points on a 3D plot.
+         -input:
+             ax: axis handle
+             X: Points coordinates.
+             strColor: Color of the text.
+             offset: Offset from the point to the text.
+         -output: None
+         """
+    for k in range(X.shape[1]):
+        ax.text(X[0, k]+offset[0], X[1, k]+offset[1], X[2,k]+offset[2], labels[k], color=strColor)
+
+def plotNumbered3DPoints(ax, X,strColor, offset):
+    """
+        Plot indexes of points on a 3D plot.
+         -input:
+             ax: axis handle
+             X: Points coordinates.
+             strColor: Color of the text.
+             offset: Offset from the point to the text.
+         -output: None
+         """
+    for k in range(X.shape[1]):
+        ax.text(X[0, k]+offset[0], X[1, k]+offset[1], X[2,k]+offset[2], str(k), color=strColor)
+
+def draw3DLine(ax, xIni, xEnd, strStyle, lColor, lWidth):
+    """
+    Draw a segment in a 3D plot
+    -input:
+        ax: axis handle
+        xIni: Initial 3D point.
+        xEnd: Final 3D point.
+        strStyle: Line style.
+        lColor: Line color.
+        lWidth: Line width.
+    """
+    ax.plot([np.squeeze(xIni[0]), np.squeeze(xEnd[0])], [np.squeeze(xIni[1]), np.squeeze(xEnd[1])], [np.squeeze(xIni[2]), np.squeeze(xEnd[2])],
+            strStyle, color=lColor, linewidth=lWidth)
+
+def drawRefSystem(ax, T_w_c, strStyle, nameStr):
+    """
+        Draw a reference system in a 3D plot: Red for X axis, Green for Y axis, and Blue for Z axis
+    -input:
+        ax: axis handle
+        T_w_c: (4x4 matrix) Reference system C seen from W.
+        strStyle: lines style.
+        nameStr: Name of the reference system.
+    """
+    draw3DLine(ax, T_w_c[0:3, 3:4], T_w_c[0:3, 3:4] + T_w_c[0:3, 0:1], strStyle, 'r', 1)
+    draw3DLine(ax, T_w_c[0:3, 3:4], T_w_c[0:3, 3:4] + T_w_c[0:3, 1:2], strStyle, 'g', 1)
+    draw3DLine(ax, T_w_c[0:3, 3:4], T_w_c[0:3, 3:4] + T_w_c[0:3, 2:3], strStyle, 'b', 1)
+    ax.text(np.squeeze( T_w_c[0, 3]+0.1), np.squeeze( T_w_c[1, 3]+0.1), np.squeeze( T_w_c[2, 3]+0.1), nameStr)
+
+if __name__ == '__main__':
+    np.set_printoptions(precision=4,linewidth=1024,suppress=True)
+
+
+    # Load ground truth
+    T_w_c1 = np.loadtxt('T_w_c1.txt')
+    T_w_c2 = np.loadtxt('T_w_c2.txt')
+
+    K_c = np.loadtxt('K_c.txt')
+    X_w = np.loadtxt('X_w.txt')
+
+    x1 = np.loadtxt('x1Data.txt')
+    x2 = np.loadtxt('x2Data.txt')
+
+
+    ##Plot the 3D cameras and the 3D points
+    fig3D = plt.figure(3)
+
+    ax = plt.axes(projection='3d', adjustable='box')
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+
+    drawRefSystem(ax, np.eye(4, 4), '-', 'W')
+    drawRefSystem(ax, T_w_c1, '-', 'C1')
+    drawRefSystem(ax, T_w_c2, '-', 'C2')
+
+    ax.scatter(X_w[0, :], X_w[1, :], X_w[2, :], marker='.')
+    plotNumbered3DPoints(ax, X_w, 'r', (0.1, 0.1, 0.1)) # For plotting with numbers (choose one of the both options)
+
+    #Matplotlib does not correctly manage the axis('equal')
+    xFakeBoundingBox = np.linspace(0, 4, 2)
+    yFakeBoundingBox = np.linspace(0, 4, 2)
+    zFakeBoundingBox = np.linspace(0, 4, 2)
+    plt.plot(xFakeBoundingBox, yFakeBoundingBox, zFakeBoundingBox, 'w.')
+    print('Close the figure to continue. Left button for orbit, right button for zoom.')
+    plt.show()
+
+    ## 2D plotting example
+    img1 = cv2.cvtColor(cv2.imread('image1.png'), cv2.COLOR_BGR2RGB)
+    img2 = cv2.cvtColor(cv2.imread('image2.png'), cv2.COLOR_BGR2RGB)
+
+
+    plt.figure(1)
+    plt.imshow(img1, cmap='gray', vmin=0, vmax=255)
+    plt.plot(x1[0, :], x1[1, :],'rx', markersize=10)
+    plotNumberedImagePoints(x1, 'r', (10,0)) # For plotting with numbers (choose one of the both options)
+    plt.title('Image 1')
+    plt.draw()  # We update the figure display
+    print('Click in the image to continue...')
+    plt.waitforbuttonpress()
+
+    plt.figure(2)
+    plt.imshow(img2, cmap='gray', vmin=0, vmax=255)
+    plt.plot(x2[0, :], x2[1, :],'rx', markersize=10)
+    plotNumberedImagePoints(x2, 'r', (10,0)) # For plotting with numbers (choose one of the both options)
+    plt.title('Image 2')
+    plt.draw()  # We update the figure display
+    print('Click in the image to continue...')
+    plt.waitforbuttonpress()
