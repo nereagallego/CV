bed6c452c9f0f2fba496d9843d75496ad9b209f3
Nerea <801950@unizar.es>
2023-11-16 13:47:26 +0100

    pnp npt work

--- labSession4/lab4.py
+++ labSession4/lab4.py
@@ -6,6 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sc
 import scipy.optimize as scOptim
+import cv2
 
 def resLineFitting(Op,xData):
     """
@@ -52,6 +53,37 @@ def drawRefSystem(ax, T_w_c, strStyle, nameStr):
     draw3DLine(ax, T_w_c[0:3, 3:4], T_w_c[0:3, 3:4] + T_w_c[0:3, 1:2], strStyle, 'g', 1)
     draw3DLine(ax, T_w_c[0:3, 3:4], T_w_c[0:3, 3:4] + T_w_c[0:3, 2:3], strStyle, 'b', 1)
     ax.text(np.squeeze( T_w_c[0, 3]+0.1), np.squeeze( T_w_c[1, 3]+0.1), np.squeeze( T_w_c[2, 3]+0.1), nameStr)
+
+def plotNumbered3DPoints(ax, X,strColor, offset):
+    """
+        Plot indexes of points on a 3D plot.
+         -input:
+             ax: axis handle
+             X: Points coordinates.
+             strColor: Color of the text.
+             offset: Offset from the point to the text.
+         -output: None
+         """
+    for k in range(X.shape[1]):
+        ax.text(X[0, k]+offset[0], X[1, k]+offset[1], X[2,k]+offset[2], str(k), color=strColor)
+
+def plotResidual(x,xProjected,strStyle):
+    """
+        Plot the residual between an image point and an estimation based on a projection model.
+         -input:
+             x: Image points.
+             xProjected: Projected points.
+             strStyle: Line style.
+         -output: None
+         """
+
+    # Plot the line between each point and its projection also plot the point with a blue dot and the projection with a red cross
+    for k in range(x.shape[0]):
+        plt.plot([x[k, 0], xProjected[k, 0]], [x[k, 1], xProjected[k, 1]], strStyle)
+        plt.plot(x[k, 0], x[k, 1], 'bo')
+        plt.plot(xProjected[k, 0], xProjected[k, 1], 'rx')
+    
+
 
 # Triangulate a set of points given the projection matrices of two cameras.
 def triangulation(P1, P2, points1, points2):    
@@ -164,29 +196,9 @@ def resBundleProjection(Op, x1Data, x2Data, K_c, nPoints):
     t = np.array([np.sin(Op[0])*np.cos(Op[1]), np.sin(Op[0])*np.sin(Op[1]), np.cos(Op[0])]).reshape(-1,1)
     theta_ext_1 = K_c @ idem
     T = np.hstack((R, t))
-    # T = np.vstack((T, np.array([0, 0, 0, 1])))
     theta_ext_2 =  K_c @ T #Proyection matrix
 
     # Compute the 3D points
-    # res = []
-    # for i in range(nPoints):
-
-    #     X_3D = np.hstack((Op[5+i*3: 5+i*3+3], np.array([1.0])))
-    #     projection1 = theta_ext_1 @ X_3D
-    #     projection1 = projection1[0:2] / projection1[2]
-    #     x1 = x1Data[0:1, i]
-    #     res1 = x1 - projection1
-
-    #     projection2 = theta_ext_2 @ X_3D
-    #     projection2 = projection2[0:2] / projection2[2]
-    #     x2 = x2Data[0:1, i]
-    #     res2 = x2 - projection2
-
-    #     res.append(res1[0])
-    #     res.append(res1[1])
-    #     res.append(res2[0])
-    #     res.append(res2[1])
-
     X_3D = np.hstack((Op[5:].reshape(-1, 3), np.ones((nPoints, 1))))
 
     projection1 = theta_ext_1 @ X_3D.T
@@ -198,12 +210,69 @@ def resBundleProjection(Op, x1Data, x2Data, K_c, nPoints):
     res2 = x2Data[:, :nPoints].T - projection2.T
 
     res = np.hstack((res1, res2)).flatten()
-    # print(res)
 
     return np.array(res)
 
+def resBundleProjection_n_cameras(Op, xData, nCameras, K_c, nPoints):
+    """
+    -input:
+    Op: Optimization parameters: this must include a
+    paramtrization for T_21 (reference 1 seen from reference 2)
+    in a proper way and for X1 (3D points in ref 1)
+    x1Data: (3xnPoints) 2D points on image 1 (homogeneous
+    coordinates)
+    x2Data: (3xnPoints) 2D points on image 2 (homogeneous
+    coordinates)
+    K_c: (3x3) Intrinsic calibration matrix
+    nPoints: Number of points
+    -output:
+    res: residuals from the error between the 2D matched points
+    and the projected points from the 3D points
+    (2 equations/residuals per 2D point)
+
+    ASSUMING AT LEAST 3 CAMERAS !!!
+    """
+
+    '''
+    Op[0:2] -> theta, phi
+    Op[2:5] -> Rx,Ry,Rz
+    Op[8:11] -> theta, phi
+    Op[11:14] -> Rx,Ry,Rz
+    ...
+    Op[] -> 3DXx,3DXy,3DXz
+    '''
+    # Bundle adjustment using least squares function
+    idem = np.append(np.eye(3), np.zeros((3, 1)), axis=1)
+    R = sc.linalg.expm(crossMatrix(Op[2:5]))
+    t = np.array([np.sin(Op[0])*np.cos(Op[1]), np.sin(Op[0])*np.sin(Op[1]), np.cos(Op[0])]).reshape(-1,1)
+    theta_ext_1 = K_c @ idem
+    T = np.hstack((R, t))
+    theta_ext_2 =  K_c @ T #Proyection matrix
+
+    theta_ext = []
+    theta_ext.append(theta_ext_1)
+    theta_ext.append(theta_ext_2)
+    for i in range(nCameras - 2):
+        R = sc.linalg.expm(crossMatrix(Op[8+6*i:11+6*i]))
+        t = np.array([np.sin(Op[6*i])*np.cos(Op[7+6*i]), np.sin(Op[6*i])*np.sin(Op[7+6*i]), np.cos(Op[6*i])]).reshape(-1,1)
+        T = np.hstack((R, t))
+        theta_ext.append(K_c @ T)
+
+    # Compute the residuals
+    res = np.array([])
+    for i in range(nCameras):
+        X_3D = np.hstack((Op[5+6*i:].reshape(-1, 3), np.ones((nPoints, 1))))
+
+        projection = theta_ext[i] @ X_3D.T
+        projection = projection[:2, :] / projection[2, :]
+        res = np.hstack((res, xData[i][:, :nPoints].T - projection.T)).flatten()
+
 if __name__ == '__main__':
     print("Hello world")
+
+    image_pers_1 = cv2.imread('image1.png')
+    image_pers_2 = cv2.imread('image2.png')
+    image_pers_3 = cv2.imread('image3.png')
 
     # PART 2
     print("PART 2")
@@ -227,6 +296,7 @@ if __name__ == '__main__':
     # Load points
     points1 = np.loadtxt('x1Data.txt')
     points2 = np.loadtxt('x2Data.txt')
+    points3 = np.loadtxt('x3Data.txt')
     worldPoints = np.loadtxt('X_w.txt')
 
     E_24 = compute_essential_matrix(F, K_c)
@@ -272,7 +342,112 @@ if __name__ == '__main__':
     Op = [0.0, 0.0, 0.0, 0.0, 0.0] + worldPoints[0:3].T.flatten().tolist()
     
     OpOptim = scOptim.least_squares(resBundleProjection, Op, args=(points1, points2, K_c, points1.shape[1]), method='lm')
+    R_c2_c1 = sc.linalg.expm(crossMatrix(OpOptim.x[2:5]))
+    t_c2_c1 = np.array([np.sin(OpOptim.x[0])*np.cos(OpOptim.x[1]), np.sin(OpOptim.x[0])*np.sin(OpOptim.x[1]), np.cos(OpOptim.x[0])]).reshape(-1,1)
+    T_c2_c1_op = np.hstack((R_c2_c1, t_c2_c1))
+    P2_op = K_c @ T_c2_c1_op
+    T_c2_c1_op = np.vstack((T_c2_c1_op, np.array([0, 0, 0, 1])))
+    points_3D_Op = np.concatenate((OpOptim.x[5: 8], np.array([1.0])), axis=0)
 
+    for i in range(worldPoints.shape[1]-1):
+        points_3D_Op = np.vstack((points_3D_Op, np.concatenate((OpOptim.x[8+3*i: 8+3*i+3], np.array([1.0])) ,axis=0)))
+
+    #### Draw 3D ################
+    fig3D = plt.figure(2)
+    ax = plt.axes(projection='3d', adjustable='box')
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+
+    drawRefSystem(ax, np.eye(4, 4), '-', 'W')
+    drawRefSystem(ax, T_w_c1, '-', 'C1')
+    #drawRefSystem(ax, wTc1 @ np.linalg.inv(c2Tc1_Op), '-', 'C2_BA')
+    drawRefSystem(ax, T_w_c1 @ np.linalg.inv(T_c2_c1_op), '-', 'C2_BA_scaled')
+    drawRefSystem(ax, T_w_c2, '-', 'C2_True')
+
+    points_Op = T_w_c1 @ (points_3D_Op).T
+
+    ax.scatter(points_Op[0, :], points_Op[1, :], points_Op[2, :], marker='.')
+    # plotNumbered3DPoints(ax, points_Op, 'b', 0.1)
+
+    ax.scatter(worldPoints[0, :], worldPoints[1, :], worldPoints[2, :], marker='.')
+    # plotNumbered3DPoints(ax, worldPoints, 'r', 0.1)
+
+    plt.title('3D points Bundle adjustment (red=True data)')
+    plt.show()
+
+    #### Plot residual bundel adj ##############
+
+    x1_p = P1 @ points_3D_Op.T
+    x2_p = P2_op @ points_3D_Op.T
+    x1_p /= x1_p[2, :]
+    x2_p /= x2_p[2, :]
+
+    plt.figure(4)
+    plt.imshow(image_pers_1, cmap='gray', vmin=0, vmax=255)
+    plt.title('Residuals after Bundle adjustment Image1')
+    print(points1.shape)
+    print(x1_p.shape)
+    print(worldPoints.shape)
+    plotResidual(points1.T, x1_p.T, 'k-')
+    # plt.plot(points1[0, :], points1[1, :], 'bo')
+    # plt.plot(x1_p[0, :], x1_p[1, :], 'rx')
+    # plotNumberedImagePoints(x1[0:2, :], 'r', 4)
+    plt.draw()
+
+    plt.show()
+
+    plt.figure(5)
+    plt.imshow(image_pers_2, cmap='gray', vmin=0, vmax=255)
+    plt.title('Residuals after Bundle adjustment Image2')
+    plotResidual(points2.T, x2_p.T, 'k-')
+    # plt.plot(points2[0, :], points2[1, :], 'bo')
+    # plt.plot(x2_p[0, :], x2_p[1, :], 'rx')
+    # plotNumberedImagePoints(x2[0:2, :], 'r', 4)
+    plt.draw()
+
+    plt.show()
+
+    # PART 3
+
+    print("PART 3")
+
+    T_w_c3 = np.loadtxt('T_w_c3.txt')
+
+    # PNP pose estimation of camera 3
+    points_3D_Scene = np.float32(points_3D_Op[:, 0:3])
+    points_C3 = np.ascontiguousarray(points3[0:2, :].T).reshape((points3.shape[1], 1, 2))
+    Coeff=[]
+    retval, rvec, tvec = cv2.solvePnP(objectPoints=points_3D_Scene, imagePoints=points_C3, cameraMatrix=K_c,
+                                      distCoeffs=np.array(Coeff), flags=cv2.SOLVEPNP_EPNP)
+    
+    R_c3_c1_pnp = sc.linalg.expm(crossMatrix(rvec))
+    t_c3_c1_pnp = tvec
+    T_c3_c1_pnp = np.hstack((R_c3_c1_pnp, t_c3_c1_pnp))
+    T_c3_c1_pnp = np.vstack((T_c3_c1_pnp, np.array([0, 0, 0, 1])))
+
+    fig3D = plt.figure(8)
+
+    ax = plt.axes(projection='3d', adjustable='box')
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+
+    drawRefSystem(ax, np.eye(4, 4), '-', 'W')
+    drawRefSystem(ax, T_w_c1, '-', 'C1_True')
+    drawRefSystem(ax, T_w_c3, '-', 'C3_True')
+    drawRefSystem(ax, T_w_c1 @ np.linalg.inv(T_c3_c1_pnp), '-', 'C3_PnP')
+    plt.title('3D camera poses PnP')
+    plt.draw()
+    plt.show()
+
+    # PART 4
+    print("PART 4")
+
+    Op = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0] + worldPoints[0:3].T.flatten().tolist()
+
+    X = np.vstack((np.vstack((points1, points2)), points3))
+    OpOptim = scOptim.least_squares(resBundleProjection_n_cameras, Op, args=(X, 3, K_c, points1.shape[1]), method='lm')
 
 
 
